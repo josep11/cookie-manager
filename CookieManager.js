c069f172bf76720d1b2098cac915f8570d56af60
Josep Alsina <josepalsinasio@gmail.com>
2022-05-16 10:39:20 +0200

    leveraging promisified to get cookies from 1.3.4

--- CookieManager.js
+++ CookieManager.js
@@ -7,26 +7,11 @@ class CookieManager {
     }
 
     /**
-    * This function shoud be in chrome-cookies-secure@1.3.2 but there is some bug and the package on npm is not the same as in github
-    * Promise wrapper for the main callback function
-    */
-    _getCookiesPromised(uri, format, profile) {
-        return new Promise((resolve, reject) => {
-            chrome.getCookies(uri, format, function (err, cookies) {
-                if (err) {
-                    return reject(err)
-                }
-                resolve(cookies)
-            }, profile)
-        })
-    }
-
-    /**
      * @returns {Array} cookies or empty array []
      */
     async getCookies(url) {
         try {
-            const cookies = await this._getCookiesPromised(url, 'puppeteer', this.chromeProfile); //3rd param is profile. Check it at "chrome://version/" and modify accordingly
+            const cookies = await chrome.getCookiesPromised(url, 'puppeteer', this.chromeProfile); //3rd param is profile. Check it at "chrome://version/" and modify accordingly
             return cookies;
         } catch (err) {
             console.error(err.name);
