8969b6ed19399925a56e759690c2c9e12d9ec36d
Josep Alsina <josep.alsina@addevent.com>
2022-10-22 19:29:18 +0200

    added types

--- CookieManager.js
+++ CookieManager.js
@@ -1,24 +1,27 @@
-const chrome = require('chrome-cookies-secure');
+const chrome = require("chrome-cookies-secure");
 
 class CookieManager {
+	constructor(chromeProfile = "Default") {
+		this.chromeProfile = chromeProfile;
+	}
 
-    constructor(chromeProfile = 'Default') {
-        this.chromeProfile = chromeProfile;
-    }
-
-    /**
-     * @returns {Array} cookies or empty array []
-     */
-    async getCookies(url) {
-        try {
-            const cookies = await chrome.getCookiesPromised(url, 'puppeteer', this.chromeProfile); //3rd param is profile. Check it at "chrome://version/" and modify accordingly
-            return cookies;
-        } catch (err) {
-            console.error(err.name);
-            throw err;
-        }
-    }
-
+	/**
+	 * @param {string} url
+	 * @returns {Promise<Array>} cookies or empty array []
+	 */
+	async getCookies(url) {
+		try {
+			const cookies = await chrome.getCookiesPromised(
+				url,
+				"puppeteer",
+				this.chromeProfile
+			); //3rd param is profile. Check it at "chrome://version/" and modify accordingly
+			return cookies;
+		} catch (err) {
+			console.error(err.name);
+			throw err;
+		}
+	}
 }
 
 module.exports = CookieManager;
